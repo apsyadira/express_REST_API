66f11adfd74cfbf5a0d14a365e9661b6b2b60443
apsyadira <apsya87@gmail.com>
2020-10-20 00:58:53 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = require("./routes");
+const app = express();
+const port = 3000;
+
+app.use("/api", router);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
